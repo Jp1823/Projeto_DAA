803e29dc137b4b329dd41c3b103bd84843e6bae4
Diogo Pereira <diogopontespereira@gmail.com>
2024-11-13 10:24:18 +0000

    New_Submissions

--- ScriptsDAA/RF_Depth10.py
+++ ScriptsDAA/RF_Depth10.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import numpy as np
+from sklearn.discriminant_analysis import StandardScaler
+from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
+from sklearn.ensemble import ExtraTreesClassifier, GradientBoostingClassifier, RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.svm import SVC
+from sklearn.pipeline import make_pipeline
 
 # Carregar datasets
-hipp_train = pd.read_csv('/mnt/c/Users/João/Documents/DAA/Projeto/train_radiomics_hipocamp.csv', na_filter=False)
-hipp_test = pd.read_csv('/mnt/c/Users/João/Documents/DAA/Projeto/test_radiomics_hipocamp.csv', na_filter=False)
+
+hipp_train = pd.read_csv('train_radiomics_hipocamp.csv', na_filter=False)
+hipp_test = pd.read_csv('test_radiomics_hipocamp.csv', na_filter=False)
 
 # Exibir informações básicas e verificar valores nulos
 print("Informações do dataset de treino:")
@@ -39,7 +44,18 @@ y_train = hipp_train_c['Transition']
 X_train_split, X_test_split, y_train_split, y_test_split = train_test_split(X_train, y_train, test_size=0.2, random_state=2022)
 
 # Treinar o modelo
-pg_model = RandomForestClassifier(bootstrap=True, max_depth=10, random_state=2022)
+# pg_model = GradientBoostingClassifier(n_estimators=500, random_state=2022, validation_fraction=0.1, n_iter_no_change=10)
+pg_model = RandomForestClassifier(bootstrap=True, max_depth=20, random_state=2022)
+# pg_model = XGBClassifier(max_depth=20, random_state=2022)
+# pg_model = make_pipeline(StandardScaler(), SVC(kernel='rbf', C=1.0, gamma='scale', random_state=2021))
+
+# Train the model
+pg_model.fit(X_train, y_train)
+
+# Predict on the test set
+y_pred = pg_model.predict(X_test_split)
+
+
 pg_model.fit(X_train_split, y_train_split)
 
 # Fazer previsões no conjunto de teste
@@ -64,6 +80,7 @@ submission_df = pd.DataFrame({
     'RowId': range(1, len(predictions_mapped) + 1), 
     'Result': predictions_mapped
 })
+
 
 if len(submission_df) < 100:
     print("Aviso: O conjunto de teste contém menos de 100 entradas. Submissão terá apenas", len(submission_df), "previsões.")
